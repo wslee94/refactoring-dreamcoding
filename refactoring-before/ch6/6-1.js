ed58cf9d0c0b32896d13ca02e31f103a8a7e9f20
wslee94 <leemuo123@gmail.com>
2022-08-23 23:40:41 +0900

    ch6 함수 추출하기 리팩터링

--- refactoring-before/ch6/6-1.js
+++ refactoring-before/ch6/6-1.js
@@ -1,9 +1,20 @@
+/*
+  문제점
+  1. 가독성이 떨어진다. (한눈에 파악이 힘들다.)
+  2. 재사용성이 떨어진다. 
+  3. 버그가 발생했을 때 발견하기 힘들다.
+
+  단계적으로 함수로 추출해나가는 리팩터링을 진행하자.
+
+  꿀팁
+  1. 지역변수는 사용하는 곳과 가까운곳에 선언하는 것이 좋다. (outstanding)
+*/
 export function printOwing(invoice) {
   let outstanding = 0;
 
-  console.log('***********************');
-  console.log('**** Customer Owes ****');
-  console.log('***********************');
+  console.log("***********************");
+  console.log("**** Customer Owes ****");
+  console.log("***********************");
 
   // calculate outstanding
   for (const o of invoice.orders) {
@@ -26,6 +37,6 @@ export function printOwing(invoice) {
 
 const invoice = {
   orders: [{ amount: 2 }, { amount: 5 }],
-  customer: '엘리',
+  customer: "엘리",
 };
 printOwing(invoice);
