3599b4b54ae68ee8994580e8a191dbbc7ddc5ef6
wslee94 <leemuo123@gmail.com>
2022-08-23 23:40:07 +0900

    ch6 함수 추출하기 리팩터링

--- refactoring-after/ch6/6-1.js
+++ refactoring-after/ch6/6-1.js
@@ -1,24 +1,33 @@
+/*
+  분리한 함수는 재사용하지 않지만 가독성이 좋아졌다.
+*/
 export function printOwing(invoice) {
-  let outstanding = 0;
+  printBanner();
+  let outstanding = calculateOutstanding(invoice);
+  recordDueDate(invoice);
+  printDetail(invoice, outstanding);
+}
 
-  console.log('***********************');
-  console.log('**** Customer Owes ****');
-  console.log('***********************');
+function printBanner() {
+  console.log("***********************");
+  console.log("**** Customer Owes ****");
+  console.log("***********************");
+}
 
-  // calculate outstanding
-  for (const o of invoice.orders) {
-    outstanding += o.amount;
-  }
+function calculateOutstanding(invoice) {
+  return invoice.orders.reduce((sum, order) => (sum += order.amount), 0);
+}
 
-  // record due date
+function recordDueDate(invoice) {
   const today = new Date();
   invoice.dueDate = new Date(
     today.getFullYear(),
     today.getMonth(),
     today.getDate() + 30
   );
+}
 
-  //print details
+function printDetail(invoice, outstanding) {
   console.log(`name: ${invoice.customer}`);
   console.log(`amount: ${outstanding}`);
   console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
@@ -26,6 +35,6 @@ export function printOwing(invoice) {
 
 const invoice = {
   orders: [{ amount: 2 }, { amount: 5 }],
-  customer: '엘리',
+  customer: "엘리",
 };
 printOwing(invoice);
