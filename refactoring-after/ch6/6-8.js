52dc65550f87371e7024b19a2d6bfdebc097ac97
wslee94 <leemuo123@gmail.com>
2022-08-27 00:00:53 +0900

    ch6 매개변수 객체 만들기 리팩터링

--- refactoring-after/ch6/6-8.js
+++ refactoring-after/ch6/6-8.js
@@ -1,24 +1,39 @@
-export function readingsOutsideRange(station, min, max) {
-  return station.readings.filter((r) => r.temp < min || r.temp > max);
+class NumberRange {
+  #min;
+  #max;
+
+  constructor(min, max) {
+    this.#min = min;
+    this.#max = max;
+  }
+
+  get min() {
+    return this.#min;
+  }
+
+  get max() {
+    return this.#max;
+  }
+
+  contains(number) {
+    return this.#min <= number && number <= this.#max;
+  }
+}
+
+export function readingsOutsideRange(station, range) {
+  return station.readings.filter((r) => !range.contains(r.temp));
 }
 
 const station = {
-  name: 'ZB1',
+  name: "ZB1",
   readings: [
-    { temp: 47, time: '2016-11-10 09:10' },
-    { temp: 53, time: '2016-11-10 09:20' },
-    { temp: 58, time: '2016-11-10 09:30' },
-    { temp: 53, time: '2016-11-10 09:40' },
-    { temp: 51, time: '2016-11-10 09:50' },
+    { temp: 47, time: "2016-11-10 09:10" },
+    { temp: 53, time: "2016-11-10 09:20" },
+    { temp: 58, time: "2016-11-10 09:30" },
+    { temp: 53, time: "2016-11-10 09:40" },
+    { temp: 51, time: "2016-11-10 09:50" },
   ],
 };
-const operationPlan = {
-  temperatureFloor: 51,
-  temperatureCeiling: 53,
-};
+const operationPlan = new NumberRange(51, 53);
 
-readingsOutsideRange(
-  station,
-  operationPlan.temperatureFloor,
-  operationPlan.temperatureCeiling
-);
+readingsOutsideRange(station, operationPlan);
