06ecf3fe2650bfb4d517b45477d6c2dcba11fc39
wslee94 <leemuo123@gmail.com>
2022-08-27 01:08:05 +0900

    ch6 변수 캡슐화 하기 리팩터링

--- refactoring-after/ch6/6-6-use.js
+++ refactoring-after/ch6/6-6-use.js
@@ -1,6 +1,4 @@
-import { getDefaultOwner } from './6-6.js';
+import { getDefaultOwner } from "./6-6.js";
 
 const owner = getDefaultOwner();
-owner.firstName = '엘리';
-console.log(owner);
-console.log(getDefaultOwner());
+owner.firstName = "엘리";
