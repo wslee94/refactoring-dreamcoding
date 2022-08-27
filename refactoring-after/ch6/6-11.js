2301c2f1165f3299df061c1dce8b839b8016e4c7
wslee94 <leemuo123@gmail.com>
2022-08-27 17:02:39 +0900

    ch6 단계 쪼개기 리팩터링

--- refactoring-after/ch6/6-11.js
+++ refactoring-after/ch6/6-11.js
@@ -1,16 +1,34 @@
 export function priceOrder(product, quantity, shippingMethod) {
-  const basePrice = product.basePrice * quantity;
-  const discount =
+  const basePrice = calculateBasePrice(quantity, product.basePrice);
+  const discount = calculateDiscount(quantity, product);
+  const shippingCost = calculateShipping(quantity, basePrice, shippingMethod);
+  return basePrice - discount + shippingCost;
+}
+
+function calculateBasePrice(quantity, basePrice) {
+  return basePrice * quantity;
+}
+
+function calculateDiscount(quantity, product) {
+  return (
     Math.max(quantity - product.discountThreshold, 0) *
     product.basePrice *
-    product.discountRate;
+    product.discountRate
+  );
+}
+
+function calculateShipping(quantity, basePrice, shippingMethod) {
+  /* 
+    shippingPerCase를 따로 함수로 분리하지 않은 이유
+    필요한건 총 배송비 이므로 굳이 분리하지 않았고, 
+    서로 연관된 데이터여서 하나의 함수(단계)로 묶음
+  */
   const shippingPerCase =
     basePrice > shippingMethod.discountThreshold
       ? shippingMethod.discountedFee
       : shippingMethod.feePerCase;
-  const shippingCost = quantity * shippingPerCase;
-  const price = basePrice - discount + shippingCost;
-  return price;
+
+  return quantity * shippingPerCase;
 }
 
 // 사용 예:
