93170bfb4acc41caa64f5302c39a4a2617f4a8c0
wslee94 <leemuo123@gmail.com>
2022-08-29 21:41:06 +0900

    ch7 레코드 캡슐화하기 리팩터링

--- refactoring-after/ch7/7-1.js
+++ refactoring-after/ch7/7-1.js
@@ -1,5 +1,30 @@
-const organization = { name: 'Acme Gooseberries', country: 'GB' };
+class Organization {
+  #name;
+  #country;
 
-organization.name = 'Dream Coding';
+  constructor(name, country) {
+    this.#name = name;
+    this.#country = country;
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  set name(value) {
+    this.#name = value;
+  }
+
+  get country() {
+    return this.#country;
+  }
+
+  set country(value) {
+    this.#country = value;
+  }
+}
+
+const organization = new Organization("Acme Gooseberries", "country");
+organization.name = "Dream Coding";
 console.log(organization.name);
 console.log(organization.country);
