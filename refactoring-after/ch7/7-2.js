db9f0886f73b49bd3b6a8651b5bc1b7d5df08fa9
wslee94 <leemuo123@gmail.com>
2022-08-29 22:17:54 +0900

    ch7 컬렉션 캡슐화하기 리팩터링

--- refactoring-after/ch7/7-2.js
+++ refactoring-after/ch7/7-2.js
@@ -11,11 +11,20 @@ export class Person {
   }
 
   get courses() {
-    return this.#courses;
+    return [...this.#courses];
   }
 
-  set courses(courses) {
-    this.#courses = courses;
+  addCourse(course) {
+    this.#courses.push(course);
+  }
+
+  removeCourse(course, runIfAbsent) {
+    const index = this.#courses.indexOf(course);
+    if (index === -1) {
+      runIfAbsent();
+      return;
+    }
+    this.#courses.splice(index, 1);
   }
 }
 
@@ -36,6 +45,15 @@ export class Course {
   }
 }
 
-const ellie = new Person('엘리');
-ellie.courses.push(new Course('리팩토링', true));
+const ellie = new Person("엘리");
+// 외부에서 배열(컬렉션) 조작을 마음대로 할 수 있음 (치명적인 냄새)
+const course = new Course("리팩토링", true);
+ellie.addCourse(course);
 console.log(ellie.courses.length);
+ellie.removeCourse(course, () => {
+  console.log("해당 코스는 존재하지 않습니다.");
+});
+console.log(ellie.courses.length);
+ellie.removeCourse(course, () => {
+  console.log("해당 코스는 존재하지 않습니다.");
+});
