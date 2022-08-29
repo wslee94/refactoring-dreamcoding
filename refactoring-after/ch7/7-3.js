e1976bdaf7f237df390aa3d35faf34bb0c58d8d6
wslee94 <leemuo123@gmail.com>
2022-08-29 23:28:34 +0900

    ch7 기본형을 객체로 바꾸기 리팩터링

--- refactoring-after/ch7/7-3.js
+++ refactoring-after/ch7/7-3.js
@@ -1,15 +1,55 @@
 export class Order {
+  #priority;
+
   constructor(data) {
-    this.priority = data.priority;
+    this.#priority = data.priority;
+  }
+
+  get priority() {
+    return this.#priority;
+  }
+}
+
+class Priority {
+  static LOW = new Priority("low", 0);
+  static NORMAL = new Priority("normal", 1);
+  static HIGH = new Priority("high", 2);
+  static RUSH = new Priority("rush", 3);
+  static legalValues() {
+    return ["low", "normal", "high", "rush"];
+  }
+
+  #name;
+  #index;
+  constructor(name, index) {
+    if (Priority.legalValues().includes(name)) {
+      this.#name = name;
+      this.#index = index;
+    } else {
+      throw Error(`${name} is not valid for Priority`);
+    }
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  get index() {
+    return this.#index;
+  }
+
+  higherThan(other) {
+    return this.#index > other.index;
   }
 }
 
 const orders = [
-  new Order({ priority: 'normal' }),
-  new Order({ priority: 'high' }),
-  new Order({ priority: 'rush' }),
+  new Order({ priority: Priority.NORMAL }),
+  new Order({ priority: Priority.HIGH }),
+  new Order({ priority: Priority.RUSH }),
 ];
 
-const highPriorityCount = orders.filter(
-  (o) => 'high' === o.priority || 'rush' === o.priority
+const highPriorityCount = orders.filter((o) =>
+  o.priority.higherThan(Priority.NORMAL)
 ).length;
+console.log(highPriorityCount);
