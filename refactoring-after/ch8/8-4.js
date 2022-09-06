1c879919e7338660aa18d2a3d7869f53c7b1f2b8
wslee94 <leemuo123@gmail.com>
2022-09-06 19:35:16 +0900

    ch8 문장을 호출한 곳으로 옮기기

--- refactoring-after/ch8/8-4.js
+++ refactoring-after/ch8/8-4.js
@@ -6,25 +6,30 @@ function renderPerson(outStream, person) {
 
 function listRecentPhotos(outStream, photos) {
   photos
-    .filter((p) => p.date > recentDateCutoff())
-    .forEach((p) => {
-      outStream.write('<div>\n');
+    .filter((photo) => photo.date > recentDateCutoff())
+    .forEach((photo) => {
+      outStream.write("<div>\n");
       emitPhotoData(outStream, p);
-      outStream.write('</div>\n');
+      outStream.write(`<p>location: ${photo.location}</p>\n`);
+      outStream.write("</div>\n");
     });
 }
 
 function emitPhotoData(outStream, photo) {
   outStream.write(`<p>title: ${photo.title}</p>\n`);
   outStream.write(`<p>date: ${photo.date.toDateString()}</p>\n`);
-  outStream.write(`<p>location: ${photo.location}</p>\n`);
 }
 
 function renderPhoto(outStream, aPhoto) {
-  outStream.write('');
+  outStream.write("");
 }
 
 function recentDateCutoff() {
   //7 days ago.
   return new Date().setDate(new Date().getDate() - 7);
 }
+
+/* 
+  공통적으로 사용되는 부분은 함수 안에,
+  사용하는 곳마다 차이가 발생하면 과감하게 함수를 호출한 쪽으로 문장을 이동시킨다.
+*/
