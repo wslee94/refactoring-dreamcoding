2b7239291d6cf737808515bbfc5857db86a8292a
wslee94 <leemuo123@gmail.com>
2022-09-14 22:40:12 +0900

    ch9 참조를 값으로 바꾸기

--- refactoring-after/ch9/9-4.js
+++ refactoring-after/ch9/9-4.js
@@ -23,7 +23,7 @@ class Person {
   }
 
   set officeAreaCode(value) {
-    this.#telephoneNumber.areaCode = value;
+    this.$telephoneNumber = new TelephoneNumber(value, this.officeNumber);
   }
 
   get officeNumber() {
@@ -31,7 +31,7 @@ class Person {
   }
 
   set officeNumber(value) {
-    this.#telephoneNumber.number = value;
+    this.telephoneNumber = new TelephoneNumber(this.officeAreaCode, value);
   }
 }
 
@@ -46,15 +46,9 @@ class TelephoneNumber {
   get areaCode() {
     return this.#areaCode;
   }
-  set areaCode(arg) {
-    this.#areaCode = arg;
-  }
 
   get number() {
     return this.#number;
-  }
-  set number(arg) {
-    this.#number = arg;
   }
 
   get toString() {
@@ -62,7 +56,7 @@ class TelephoneNumber {
   }
 }
 
-const person = new Person('엘리', '010', '12345678');
+const person = new Person("엘리", "010", "12345678");
 console.log(person.name);
 console.log(person.officeAreaCode);
 console.log(person.officeNumber);
