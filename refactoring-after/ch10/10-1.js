0ca9408e43eb2a3e9a0328c156afde9156f5cd9d
wslee94 <leemuo123@gmail.com>
2022-09-17 16:38:17 +0900

    ch10 조건문 분해하기

--- refactoring-after/ch10/10-1.js
+++ refactoring-after/ch10/10-1.js
@@ -1,7 +1,39 @@
 function calculateCharge(date, quantity, plan) {
-  let charge = 0;
-  if (!date.isBefore(plan.summerStart) && !date.isAfter(plan.summerEnd))
-    charge = quantity * plan.summerRate;
-  else charge = quantity * plan.regularRate + plan.regularServiceCharge;
-  return charge;
+  const isSummer =
+    !date.isBefore(plan.summerStart) && !date.isAfter(plan.summerEnd);
+  const summerCharge = quantity * plan.summerRate;
+  const regularCharge = quantity * plan.regularRate + plan.regularServiceCharge;
+  return isSummer ? summerCharge : regularCharge;
+}
+
+function calculateChargeWithClass(date, quantity, plan) {
+  const charge = new Charge(date, quantity, plan);
+  return charge.isSummer ? charge.summerCharge : charge.regularCharge;
+}
+
+class Charge {
+  #date;
+  #quantity;
+  #plan;
+
+  constructor(date, quantity, plan) {
+    this.#date = date;
+    this.#quantity = quantity;
+    this.#plan = paln;
+  }
+
+  get isSummer() {
+    return (
+      !this.#date.isBefore(this.#plan.summerStart) &&
+      !this.#date.isAfter(this.#plan.summerEnd)
+    );
+  }
+
+  get summerCharge() {
+    return this.#quantity * this.#plan.summerRate;
+  }
+
+  get regularCharge() {
+    this.#quantity * this.#plan.regularRate + this.#plan.regularServiceCharge;
+  }
 }
