9208ee4140536e971c904602328fb9002ce2547a
wslee94 <leemuo123@gmail.com>
2022-09-17 21:05:59 +0900

    ch10 특이 케이스 추가하기

--- refactoring-after/ch10/10-5.js
+++ refactoring-after/ch10/10-5.js
@@ -4,7 +4,9 @@ export class Site {
   }
 
   get customer() {
-    return this._customer;
+    return this._customer === "unknown"
+      ? new UnknownCustomer()
+      : new Customer(this._customer);
   }
 }
 
@@ -30,12 +32,18 @@ export class Customer {
   }
 }
 
+export class UnknownCustomer extends Customer {
+  get name() {
+    return "occupant";
+  }
+}
+
 // 사용하는 부분
 export function customerName(site) {
   const aCustomer = site.customer;
   // 더 많은 코드가 여기에 있음
   let customerName;
-  if (aCustomer === 'unknown') customerName = 'occupant';
+  if (aCustomer === "unknown") customerName = "occupant";
   else customerName = aCustomer.name;
 
   return customerName;
