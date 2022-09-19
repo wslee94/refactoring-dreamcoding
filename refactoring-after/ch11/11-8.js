1bdb67884e8540e5541a9e12bf00796860d2a34d
wslee94 <leemuo123@gmail.com>
2022-09-19 22:26:13 +0900

    ch11 생성자를 팩터리 함수로 바꾸기

--- refactoring-after/ch11/11-8.js
+++ refactoring-after/ch11/11-8.js
@@ -1,4 +1,5 @@
 export class Employee {
+  // 타입스크립트에서는 생성자를 private로 선언할 수 있음 (외부에서 생성자 호출 못함)
   constructor(name, typeCode) {
     this._name = name;
     this._typeCode = typeCode;
@@ -12,6 +13,20 @@ export class Employee {
   }
 
   static get legalTypeCodes() {
-    return { E: 'Engineer', M: 'Manager', S: 'Salesman' };
+    return { E: "Engineer", M: "Manager", S: "Salesman" };
+  }
+
+  static createEngineer(name) {
+    return new Employee(name, "E");
+  }
+
+  static createManager(name) {
+    return new Employee(name, "M");
+  }
+
+  static createSalesman(name) {
+    return new Employee(name, "S");
   }
 }
+
+Employee.createEngineer("wslee");
