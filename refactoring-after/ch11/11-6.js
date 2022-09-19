41fe7bbf7bd15fd82fa9918b69936bde29bf51ae
wslee94 <leemuo123@gmail.com>
2022-09-19 20:58:26 +0900

    ch11 질의 함수를 매개변수로 바꾸기

--- refactoring-after/ch11/11-6.js
+++ refactoring-after/ch11/11-6.js
@@ -1,7 +1,8 @@
-targetTemperature(aPlan);
+targetTemperature(aPlan, thermostat.currentTemperature);
 
 // 다른모듈에 있는 함수라고 가정
-function targetTemperature(aPlan) {
-  currentTemperature = thermostat.currentTemperature;
+function targetTemperature(plan, currentTemperature) {
   // ...
 }
+
+// thermostat -> 전역적인 혹은 외부 스코프 객체에 직접 접근하는 것은, 결합도가 매우 높다.
