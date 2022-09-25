4fc39a0fd38885f69f6a95de7f1f81b61e05877a
wslee94 <leemuo123@gmail.com>
2022-09-25 21:22:30 +0900

    ch12 서브클래스 제거하기

--- refactoring-after/ch12/12-7.js
+++ refactoring-after/ch12/12-7.js
@@ -1,7 +1,9 @@
 class Person {
   #name;
-  constructor(name) {
+  #genderCode;
+  constructor(name, genderCode) {
     this.#name = name;
+    this.#genderCode = genderCode;
   }
 
   get name() {
@@ -9,45 +11,39 @@ class Person {
   }
 
   get genderCode() {
-    return 'X';
+    return this.#genderCode;
   }
-}
 
-class Male extends Person {
-  get genderCode() {
-    return 'M';
+  /* 관련 로직을 클래스 내부에서 제공하자 (응집도 ↑) */
+  get isMail() {
+    return this.genderCode === "M";
   }
-}
 
-class Female extends Person {
-  get genderCode() {
-    return 'F';
+  /* 복작합 생성 스크립트는 팩터리 메서드를 이용해서 제공하자. (응집도 ↑) */
+  static create(record) {
+    switch (record.gender) {
+      case "M":
+        return new Person(record.name, "M");
+      case "F":
+        return new Person(record.name, "F");
+      default:
+        return new Person(record.name, "X");
+    }
   }
 }
 
 function loadFromInput(data) {
   const result = [];
   data.forEach((record) => {
-    let person;
-    switch (record.gender) {
-      case 'M':
-        person = new Male(record.name);
-        break;
-      case 'F':
-        person = new Female(record.name);
-        break;
-      default:
-        person = new Person(record.name);
-    }
-    result.push(person);
+    result.push(Person.create(record));
   });
   return result;
 }
 
 const people = loadFromInput([
-  { name: '엘리', gender: 'F' },
-  { name: '철수', gender: 'M' },
-  { name: '밥', gender: 'M' },
+  { name: "엘리", gender: "F" },
+  { name: "철수", gender: "M" },
+  { name: "밥", gender: "M" },
 ]);
-const numberOfMales = people.filter((p) => p instanceof Male).length;
+const numberOfMales = people.filter((p) => p.isMail).length;
 console.log(numberOfMales);
