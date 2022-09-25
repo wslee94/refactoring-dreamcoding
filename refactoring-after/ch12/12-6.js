bdfa2a62c24240b4d8fd4680594e7b8061ee1f1b
wslee94 <leemuo123@gmail.com>
2022-09-25 20:58:15 +0900

    ch12 타입 코드를 서브클래스로 바꾸기

--- refactoring-after/ch12/12-6.js
+++ refactoring-after/ch12/12-6.js
@@ -1,26 +1,41 @@
 class Employee {
   #name;
-  #type;
   constructor(name, type) {
-    this.validateType(type);
     this.#name = name;
-    this.#type = type;
-  }
-
-  validateType(arg) {
-    if (!['engineer', 'manager', 'salesperson'].includes(arg)) {
-      throw new Error(`${arg}라는 직원 유형은 없습니다.`);
-    }
   }
 
   get type() {
-    return this.#type;
+    return "employee";
   }
 
   toString() {
     return `${this.#name} (${this.type})`;
   }
+
+  // 만약 서버 데이터 JSON 형태로 데이터를 받아올 경우 아래 메서드를 통해 생성
+  #createEmployee(name, type) {
+    switch (type) {
+      case "engineer":
+        return new Engineer(name);
+      case "salesperson":
+        return new Manager(name);
+      default:
+        throw new Error(`${type}라는 직원 유형은 없습니다.`);
+    }
+  }
 }
 
-const ellie = new Employee('엘리', 'engineer');
-const bob = new Employee('밥', 'manager');
+class Engineer extends Employee {
+  get type() {
+    return "engineer";
+  }
+}
+
+class Manager extends Employee {
+  get type() {
+    return "manager";
+  }
+}
+
+const ellie = new Engineer("엘리");
+const bob = new Manager("밥");
