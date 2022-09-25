a8d598da844f8909a550f75f3b1973dd5f35a2be
wslee94 <leemuo123@gmail.com>
2022-09-25 20:34:28 +0900

    ch12 필드 올리기

--- refactoring-after/ch12/12-2.js
+++ refactoring-after/ch12/12-2.js
@@ -1,9 +1,6 @@
-class Employee {}
-
-class Salesperson extends Employee {
+class Employee {
   #name;
 }
 
-class Engineer extends Employee {
-  #name;
-}
+class Salesperson extends Employee {}
+class Engineer extends Employee {}
